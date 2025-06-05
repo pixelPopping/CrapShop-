7e50fee89f1da7c55f6a7e722d7203d133681075
pixelPopping <sydney-cook@outlook.com>
2025-06-05 17:57:21 +0200

    werken aan het custom component

---
+++ src/components/ui/wheelspin.js
@@ -0,0 +1,22 @@
+//stap 1 functie scrhijven voor de wheel of fortune
+//stap 2 array van items
+//stap 3 ul list maken items in wheel spin
+//stap 4 wheel of fortune interactief maken
+//stap 5 spin button maken en interactief maken
+
+function wheelOfFortune() {
+    let wheelOfFortune = ["ManChlotes", "FemaleChlotes", "Jewlery", "Electronics", "Vintage",];
+
+    for (let i = 0; i < wheelOfFortune.length; i++) {
+     console.log(wheelOfFortune[i]);
+    }
+}
+
+wheelOfFortune();
+console.log(wheelOfFortune);
+
+
+
+
+
+
