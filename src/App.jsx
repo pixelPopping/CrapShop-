7d96d2887b8dc6a08e3667e4976b56bf40da74f3
pixelPopping <sydney-cook@outlook.com>
2025-06-05 17:54:57 +0200

    werken aan de wheel of fortune feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
+import './App.css';
+import { useState } from 'react';
 
-import './App.css'
 
 function App() {
+  const [results, setResults] = useState('')
+  const [spin, setSpin] = useState('')
 
+  function wheelOfFortune() {
+    let wheelOfFortune = ["ManChlotes", "FemaleChlotes", "Jewlery", "Electronics", "Vintage",];
+    let i = Math.floor(Math.random() * wheelOfFortune.length);
+    let selectedItem  = wheelOfFortune[i];
+    setResults(selectedItem);
+
+  }
   return (
-      <h1>CrapShop</h1>
-  )
+      <section className="App">
+        <div  className="animated-box">
+          <ul>
+            <li className="item">"ManChlotes"</li>
+            <li className="item">"FemaleChlotes"</li>
+            <li className="item">"Jewlery"</li>
+            <li className="item">"Electronics"</li>
+            <li className="item">"Vintage"</li>
+            <button onClick={wheelOfFortune}>spin</button>
+            {results && <p>You got: <strong>{results}</strong></p>}
+          </ul>
+        </div>
+      </section>
+
+  );
 }
 
 export default App
+
