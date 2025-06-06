5766bc1d954baf570f20bb1c1b8229896adf8f8e
pixelPopping <sydney-cook@outlook.com>
2025-06-06 18:51:57 +0200

    vechten met de spin animatie

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+//prioriteiten
+// 1 wheel of fortune
+// 2 shopping cart
+// 3 registreren en inloggen
+// 4 zoek filter/ categorieen box
+
+
 import './App.css';
 import { useState } from 'react';
 
@@ -5,41 +12,44 @@ import { useState } from 'react';
 function App() {
   const [results, setResults] = useState('')
   const [spin, setSpin] = useState(3)
+  const [spinning, setSpinng] = useState(false);
 
   function wheelOfFortune() {
     if (spin <= 0) return;
     let wheelOfFortune = ["ManChlotes", "FemaleChlotes", "Jewlery", "Electronics", "Vintage",];
     let i = Math.floor(Math.random() * wheelOfFortune.length);
     let selectedItem = wheelOfFortune[i];
-    setResults(selectedItem);
 
 
+    setSpinng(true)
 
-
-    setSpin(prev => prev -1);
-
-   // if (spin >= 3) {
-     /// setSpin(0)
-    //}
+    setTimeout(() => {
+      setSpinng(false)
+      setResults(selectedItem);
+      setSpin(prev => prev - 1);
+    }, 2000)
   }
+
   return (
       <>
       <section className="App">
         <div  className="animated-box">
           <ul>
-            <li className="item">"ManChlotes"</li>
-            <li className="item">"FemaleChlotes"</li>
-            <li className="item">"Jewlery"</li>
-            <li className="item">"Electronics"</li>
-            <li className="item">"Vintage"</li>
+
           </ul>
           <button onClick={wheelOfFortune} disabled={spin <= 0}>spin ({spin} left)</button>
           {results && <p>You got: <strong>{results}</strong></p>}
 
         </div>
       </section>
-        <div className="container">
+        <div className={`wheel ${spinning ? 'spinning' : ''}`}>
+          <li className="item">"ManChlotes"</li>
+          <li className="item">"FemaleChlotes"</li>
+          <li className="item">"Jewlery"</li>
+          <li className="item">"Electronics"</li>
+          <li className="item">"Vintage"</li>
         </div>
+
 
       </>
   );
