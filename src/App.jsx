bca07b1f95e93c198ebb4284c134b201099f853c
pixelPopping <sydney-cook@outlook.com>
2025-06-06 16:44:31 +0200

    werken aan een functie om de button disabled als de gebruiker zijn 3 spins verbruikt heeft

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,23 @@ import { useState } from 'react';
 
 function App() {
   const [results, setResults] = useState('')
-  const [spin, setSpin] = useState('')
+  const [spin, setSpin] = useState(3)
 
   function wheelOfFortune() {
+    if (spin <= 0) return;
     let wheelOfFortune = ["ManChlotes", "FemaleChlotes", "Jewlery", "Electronics", "Vintage",];
     let i = Math.floor(Math.random() * wheelOfFortune.length);
-    let selectedItem  = wheelOfFortune[i];
+    let selectedItem = wheelOfFortune[i];
     setResults(selectedItem);
 
+
+
+
+    setSpin(prev => prev -1);
+
+   // if (spin >= 3) {
+     /// setSpin(0)
+    //}
   }
   return (
       <>
@@ -23,9 +32,10 @@ function App() {
             <li className="item">"Jewlery"</li>
             <li className="item">"Electronics"</li>
             <li className="item">"Vintage"</li>
-            <button onClick={wheelOfFortune}>spin</button>
-            {results && <p>You got: <strong>{results}</strong></p>}
           </ul>
+          <button onClick={wheelOfFortune} disabled={spin <= 0}>spin ({spin} left)</button>
+          {results && <p>You got: <strong>{results}</strong></p>}
+
         </div>
       </section>
         <div className="container">
