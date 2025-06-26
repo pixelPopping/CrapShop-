e979cf803924219cff9e39af5d75df43402bc585
pixelPopping <sydney-cook@outlook.com>
2025-06-26 17:42:43 +0200

    zoekbalk werkt maar moet nog aangewerkt worden

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,45 +1,51 @@
-import axios from 'axios';
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import './Shop.css';
+import ZoekBalk from '../components/searchfilter/ZoekBalk';
+import axios from 'axios';
 
 function Shop() {
     const [shop, setShop] = useState([]);
-    const [error, setError] = useState(false);
-    const [loading, setLoading] = useState(false);
+    const [query, setQuery] = useState("");
+    const [filterdEvents, setFilterdEvents] = useState([]);
 
     useEffect(() => {
-     async function  getData() {
-         setError(false);
+        async function getData() {
             try {
                 const response = await axios.get("https://fakestoreapi.com/products");
                 console.log(response.data);
                 setShop(response.data);
+                setFilterdEvents(response.data);
             } catch (e) {
                 console.error(e);
-                error(true);
-            } finally {
-
             }
         }
-            getData();
-        })
+        getData();
+    }, []);
+
+    const FilteredEvent = shop.filter((item) => {
+        const matched = item.title.toLowerCase().includes(query.toLowerCase()) || item.description.toLowerCase().includes(query.toLowerCase())
+        return matched;
+    });
+
+    return (
+        <div className="shop">
+            <h1>zoekBalk</h1>
+            <ZoekBalk
+                type="tekst"
+                inputValue={query}
+                inputCallback={setQuery}
+            />
 
 
-        return (
-            <div className="shop">
-                <h1>Shop Producten</h1>
-
-                <ul>
-                    {shop.map((item) => (
-                        <li key={item.id}>
-                            <h3>{item.title}</h3>
-                            <p>{item.description}</p>
-                            <img src={item.image} alt={item.title} style={{width: "100px"}}/>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        );
+            {FilteredEvent.map((item, index) => (
+                <li key={index}>
+                    <h3>{item.title}</h3>
+                    <p>{item.description}</p>
+                    <img src={item.image} alt={item.title} />
+                </li>
+            ))}
+        </div>
+    );
 }
 
 export default Shop;
