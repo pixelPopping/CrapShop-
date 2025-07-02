e85056c4c6cfe45529dad6abbc41275dbd45451e
pixelPopping <sydney-cook@outlook.com>
2025-07-02 23:43:39 +0200

    werken aan shopcard component voor alle logica met usenavigate

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -3,12 +3,20 @@ import './Shop.css';
 import ZoekBalk from '../../components/searchFilter/ZoekBalk.jsx';
 import axios from 'axios';
 import Shopcard from "../../components/shopcard/Shopcard.jsx";
+import { useNavigate } from "react-router-dom";
+import Navigation from "../../components/navbar/Navigation.jsx";
+
+//1 data ophalen met een button en loggen in de console
+//2 als je juiste data heb opslaan in de state
+//3 mounth toevoegen zodat data automatisch renderd
+//4 error en loading state toevoegen
 
 function Shop() {
     const [shop, setShop] = useState([]);
     const [query, setQuery] = useState("");
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
+    const navigate = useNavigate();
 
     useEffect(() => {
         setLoading(true);
@@ -16,8 +24,8 @@ function Shop() {
         async function getData() {
             try {
                 const response = await axios.get("https://fakestoreapi.com/products");
-                console.log(response.data);
                 setShop(response.data);
+                console.log(response.data);
             } catch (e) {
                 setError(true);
                 console.error(e);
@@ -28,12 +36,14 @@ function Shop() {
         getData();
     }, []);
 
-    const filteredItems = shop.filter((item) => {
-        return (
-            item.title.toLowerCase().includes(query.toLowerCase()) ||
-            item.description.toLowerCase().includes(query.toLowerCase())
-        );
-    });
+    const filteredItems = shop.filter((item) =>
+        item.title.toLowerCase().includes(query.toLowerCase()) ||
+        item.description.toLowerCase().includes(query.toLowerCase())
+    );
+
+    //function onLinkClick(item) {
+       // navigate(`/detailpagina/${item.id}`);
+    //}
 
     return (
         <>
@@ -41,21 +51,22 @@ function Shop() {
             {error && <p>Er ging iets mis bij het ophalen van de producten.</p>}
             {!loading && !error && (
                 <div className="outer-container">
-                    <h1>ZoekBalk</h1>
+                    <h1>Shop</h1>
                     <ZoekBalk
                         type="tekst"
                         inputValue={query}
                         inputCallback={setQuery}
                     />
                     <div className="inner-container">
-                    {filteredItems.map((item) => (
-                        <Shopcard
-                            key={item.id}
-                            label={item.title}
-                            text={item.description}
-                            image={item.image}
-                        />
-                    ))}
+                        {filteredItems.map((item) => (
+                            <Shopcard
+                                key={item.id}
+                                onClick={() => navigate(`/detailpagina/${item.id}`)}
+                                label={item.title}
+                                text={item.description}
+                                image={item.image}
+                            />
+                        ))}
                     </div>
                 </div>
             )}
@@ -64,6 +75,7 @@ function Shop() {
 }
 
 export default Shop;
+
 
 
 
