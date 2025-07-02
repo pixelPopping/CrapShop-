c1a35444c93d8c345b2dee1ad515220ac6b5a9e9
pixelPopping <sydney-cook@outlook.com>
2025-07-02 23:41:21 +0200

    werken aan de Detailpagina zorgen dat als je in de shop op product klik de details ziet

--- src/pages/detailPagina/DetailPagina.jsx
+++ src/pages/detailPagina/DetailPagina.jsx
@@ -1,19 +1,66 @@
 import {useParams, useNavigate} from "react-router-dom";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import ZoekBalk from "../../components/searchFilter/ZoekBalk.jsx";
+import DetailCard from "../../components/detailcard/DetailCard.jsx";
 
 function DetailPagina () {
-    const { productId } = useParams();
+    const [shop, setShop] = useState([]);
+    const [query, setQuery] = useState("");
+    const { id } = useParams();
     const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setLoading(true);
+        setError(false);
+        async function ShopDetails() {
+            try {
+                const response = await axios.get(`https://fakestoreapi.com/products/${id}`);
+                setShop(response.data);
+            } catch (e) {
+                setError(true);
+                console.error(e);
+            } finally {
+                setLoading(false);
+            }
+        }
+        ShopDetails();
+    }, [id]);
 
     function handleNavigate() {
         navigate("/shop");
     }
 
+
+
     return (
         <>
-        <h1>DetailPagina Page {productId}</h1>
-    <h2 onClick={handleNavigate}>terug naar blog Shop</h2>
-        </>
-    )
+            {loading && <p>Bezig met laden...</p>}
+            {error && <p>Er ging iets mis bij het ophalen van de producten.</p>}
+
+            {!loading && !error && shop && (
+        <div className="outer-container">
+            <ZoekBalk
+                type="tekst"
+                inputValue={query}
+                inputCallback={setQuery}
+            />
+            <div className="inner-container">
+                {shop && (
+                    <DetailCard
+                        key={shop.id}
+                        label={shop.title}
+                        text={shop.description}
+                        image={shop.image}
+                    />
+                    )}
+            </div>
+        </div>
+    )}
+</>
+);
 }
 
 export default DetailPagina;
