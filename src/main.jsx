11d8c7ad1d684d7c15e558ce82d9c4366f25c1f1
pixelPopping <sydney-cook@outlook.com>
2025-07-20 19:43:12 +0200

    opzetten van context

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from "react-router-dom";
+import ShoppingCartProvider, {ShoppingCartContext} from "./components/context/ShoppingCartContext";
+
+
 
 createRoot(document.getElementById('root')).render(
 <StrictMode>
 <BrowserRouter>
+    <ShoppingCartProvider>
     <App />
+    </ShoppingCartProvider>
 </BrowserRouter>
 </StrictMode>
 )
