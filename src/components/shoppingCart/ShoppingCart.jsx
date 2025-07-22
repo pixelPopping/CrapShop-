4f54a1ac7d0e2cc66bb5991dc1268099195acea1
pixelPopping <sydney-cook@outlook.com>
2025-07-22 19:29:23 +0200

    zorgen dat de context all het denkwerk doet en het component voert het ui

--- src/components/shoppingCart/ShoppingCart.jsx
+++ src/components/shoppingCart/ShoppingCart.jsx
@@ -6,38 +6,48 @@ import {ShoppingCartContext} from "../context/ShoppingCartContext.jsx";
 
 //({ resetButton, cartItems})
 
-function ShoppingCart({ resetButton, cartItems}){
 
+function ShoppingCart() {
+const {items, price, lengthcart, reSet} = useContext(ShoppingCartContext);
 
-   // const { id } = useParams();
+    //const { id } = useParams();
 
-    //useEffect(() => {
-        //async function fetchProduct() {
-           // try {
+   // useEffect(() => {
+       // async function fetchProduct() {
+         //   try {
                // const response = await axios.get(`https://fakestoreapi.com/products/${id}`);
-               // setProduct(response.data);
-           // } catch (e) {
+              //  setProduct(response.data);
+          //  } catch (e) {
                // console.error(e);
           //  }
-       // }
-       // fetchProduct();
-   // }, [id]);
+//}
+      //  fetchProduct();
+   //}, [id]);
 
     //function reset() {
-       // setCartItems([]);
+     //   setCartItems([]);
+   //}
+
+
+    //function totaalPrijs() {
+    //   return cartItems.reduce((totaal, item) => totaal + item.price, 0);
+    //}
+
+
+    ///functie schrijven met reduce om de lengte van de shoppingcart te berekenen
+
+
+   // function totaalAantal() {
+   //     return cartItems.reduce((aantal, item) => aantal + 1, 0);
    // }
 
 
-    function totaalPrijs() {
-       return cartItems.reduce((totaal, item) => totaal + item.price, 0);
-    }
 
+    // function addToCart() {
+      //  if (product)
+          //  items([...items, product]);
+       // }
 
-    //function addToCart() {
-       // if (product) {
-          //  setCartItems([...cartItems, product]);
-        //}
-    //}
 
     return (
         <>
@@ -45,17 +55,16 @@ function ShoppingCart({ resetButton, cartItems}){
 
             <h2>Winkelwagen</h2>
             <ul>
-                {cartItems.length === 0 && <li>leeg</li>}
-                {cartItems.map((item) => (
+                {items.length === 0 && <li>leeg</li>}
+                {items.map((item) => (
                     <li key={item.id}>
-                        {item.title} - €{item.price}
+                        {item.title}  {item.price}
                     </li>
                 ))}
             </ul>
-            <p><strong>Totaalprijs:</strong> €{totaalPrijs().toFixed(2)}</p>
-            <button onClick={resetButton}>reset</button>
-
-    />
+            <p><strong>Totaalprijs:</strong> €{price().toFixed(2)}</p>
+            <p><strong>Totaalproducten: {lengthcart()}</strong></p>
+            <button onClick={()=> reSet()}>reset</button>
             </>
     );
 }
