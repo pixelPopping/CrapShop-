4ca8e218e67e475868e89c62d96aa601e2727806
pixelPopping <sydney-cook@outlook.com>
2025-08-06 19:44:43 +0200

    toevoegen van favorite button en zorgen dat dit gelinkt is aan de favoriet pagina

--- src/components/detailcard/DetailCard.jsx
+++ src/components/detailcard/DetailCard.jsx
@@ -1,13 +1,18 @@
 import { useState, useContext } from "react";
 import { ShoppingCartContext } from "../context/ShoppingCartContext";
+import { FavoriteContext } from "../context/FavoriteContext";
 import Dropdown from "../../components/dropdown/Dropdown";
 
 function DetailCard({ id, label, text, image, price }) {
-    const { cart, items } = useContext(ShoppingCartContext);
+    const { cart, items: cartItems } = useContext(ShoppingCartContext);
+    const {  toggleFavorite, items: favorieten } = useContext(FavoriteContext);
+
     const [selectedQuantity, setSelectedQuantity] = useState(1);
 
-    const itemInCart = items.find(item => item.id === id);
+    const itemInCart = cartItems.find(item => item.id === id);
     const quantityInCart = itemInCart?.quantity || 0;
+
+    const isFavoriet = favorieten.some(item => item.id === id);
 
     return (
         <div className="detail-card">
@@ -23,8 +28,6 @@ function DetailCard({ id, label, text, image, price }) {
             <Dropdown
                 value={selectedQuantity}
                 onChange={setSelectedQuantity}
-                max={10}
-                label="Aantal"
             />
 
             <button
@@ -42,12 +45,28 @@ function DetailCard({ id, label, text, image, price }) {
                 Voeg toe
             </button>
 
+            <button
+                onClick={() =>
+                    toggleFavorite({
+                        id,
+                        label,
+                        text,
+                        image,
+                        price,
+                        quantity: selectedQuantity
+                    })
+                }
+            >
+                {isFavoriet ? "💖 Verwijder uit favorieten" : "🤍 Voeg toe aan favorieten"}
+            </button>
+
             <p>Aantal in winkelwagen: {quantityInCart}</p>
         </div>
     );
 }
 
 export default DetailCard;
+
 
 
 
