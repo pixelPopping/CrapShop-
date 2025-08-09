51a37192fb5d78c25823c6824ae53f3e10898e4b
pixelPopping <sydney-cook@outlook.com>
2025-08-09 19:54:52 +0200

    fallback toevoegen aan de img daar lag de fout niet

--- src/components/shopcard/Shopcard.jsx
+++ src/components/shopcard/Shopcard.jsx
@@ -1,19 +1,24 @@
 import './Shopcard.css';
 import { useNavigate } from 'react-router-dom';
+import React from "react";
 
-function Shopcard({ id, label, text, image, onClick }) {
+function Shopcard({ id, label, text, image, onClick,  rating}) {
     const navigate = useNavigate();
 
-   // function handleClick() {
-       // navigate(`/detailpagina/${id}`);
+    // function handleClick() {
+    // navigate(`/detailpagina/${id}`);
     //}
 
     return (
         <article className="card" onClick={onClick}>
             <h3>{label} {id}</h3>
             <img src={image} alt={label} />
+            <p>{rating} ⭐</p>
         </article>
     );
 }
 
 export default Shopcard;
+
+
+
